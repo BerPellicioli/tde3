66f2172e6b9f8c4f93af3f72af577f6f211a8403
Bernardo Pellicioli <berpellicioli07@gmail.com>
2024-07-06 19:41:56 -0300

    notificação push

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:tde3/firebase_options.dart';
 import 'call_api.dart';
 import 'consult_api_extended.dart';
+import 'firebase_api.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await FirebaseApi().initNotifications();
   runApp(const MyApp());
 }
 
